84c93fab9dd3f3db43b17a23095fd000eb9b943b
Maicon Souza <maicon_souza09@hotmail.com>
2025-01-13 22:17:22 -0300

    parte 5 tomando decisoes

---
+++ fullstack_django/18_javascript_basico/modulo_3/imc.js
@@ -0,0 +1,7 @@
+//peso / altura ** 2
+
+const imc = 105 / (1.70 ** 2)
+const imc2 = 65 / (1.70 ** 2)
+
+console.log(imc)
+console.log(imc2)
---
+++ fullstack_django/18_javascript_basico/modulo_3/regraDe3.js
@@ -0,0 +1,7 @@
+// Regra de 3
+// tinham 420 pessoas em studio
+// Porém apenas 135 retornaram para o studio
+// Quantos % das pessoas retornaram
+const result = (135 * 100) / 420
+
+console.log(result) //32.142857142857146
